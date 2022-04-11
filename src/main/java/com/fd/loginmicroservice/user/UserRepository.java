5176c078fbebc0c424ac59de8e904de63f4b8381
Prasun Mitra <pmitra@firstderivatives.com>
2022-04-11 19:49:40 +0100

    upload java loginservice code..

---
+++ src/main/java/com/fd/loginmicroservice/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fd.loginmicroservice.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+
+}
---
+++ src/test/java/com/fd/loginmicroservice/UserRepositoryTests.java
@@ -0,0 +1,44 @@
+package com.fd.loginmicroservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.hibernate.mapping.PrimaryKey;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.fd.loginmicroservice.user.User;
+import com.fd.loginmicroservice.user.UserRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	
+	public void testCreateUser() {
+		
+		User user = new User();
+		user.setEmail("prasunmitragcp@gmail.com");
+		user.setPassword("Prasun@123");
+		user.setFirstname("Prasun");
+		user.setLastname("Mitra");
+		
+		User savedUser =  repo.save(user);
+		
+		User existUser = entityManager.find(User.class, savedUser.getId());
+		
+		assertEquals(existUser.getEmail(), user.getEmail());
+		
+	}
+
+}
